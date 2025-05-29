4d870e32a36c708ce27892415e6ba27d1e96b1ff
MeteorClear <tivics9973@gmail.com>
2025-05-29 20:20:30 +0900

    [#12] 관련 파일 생성

---
+++ DocsSearcher/XmlUtils.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"  // MFC 헤더
+#include "XmlUtils.h"
+
+
+// ─── 내부 함수 ───
+namespace
+{
+
+} // namespace
+
+// ─── 외부 함수 ───
---
+++ DocsSearcher/XmlUtils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef DOCSSEARCHER_XML_UTILS_H_
+#define DOCSSEARCHER_XML_UTILS_H_
+
+namespace docssearcher {
+
+	
+
+}  // namespace docssearcher
+
+#endif  // DOCSSEARCHER_XML_UTILS_H_
