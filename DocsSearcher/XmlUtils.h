6e67ec1d2ee9683dd05e897a8fa6cb846becf6ce
MeteorClear <tivics9973@gmail.com>
2025-05-30 18:25:05 +0900

    [#13] 헤더에 선언 추가

--- DocsSearcher/XmlUtils.h
+++ DocsSearcher/XmlUtils.h
@@ -15,5 +15,7 @@
 
 std::wstring ExtractDocxText(const std::wstring& filepath);  // .docx 파일에서 데이터 추출 함수
 std::wstring ExtractHwpxText(const std::wstring& filepath);  // .hwpx 파일에서 데이터 추출 함수
+bool SearchKeywordInDocx(const CString& file_path, const CString& keyword, CString& context);  // .docx 파일에서 키워드 검색 함수
+bool SearchKeywordInHwpx(const CString& file_path, const CString& keyword, CString& context);  // .hwpx 파일에서 키워드 검색 함수
 
 #endif  // DOCSSEARCHER_XML_UTILS_H_
